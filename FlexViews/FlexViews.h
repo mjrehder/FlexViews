fd3eef97fe78fa7e25bc30d60fe3be8ace4a536e
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-05 13:07:55 +0200

    Initial Commit

---
+++ FlexViews/FlexViews.h
@@ -0,0 +1,18 @@
+//
+//  FlexViews.h
+//  FlexViews
+//
+//  Created by Martin Rehder on 05.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlexViews.
+FOUNDATION_EXPORT double FlexViewsVersionNumber;
+
+//! Project version string for FlexViews.
+FOUNDATION_EXPORT const unsigned char FlexViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexViews/PublicHeader.h>
+
+
