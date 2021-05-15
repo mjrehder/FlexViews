bea0b59f810808b7257a6330f0eed8589032b410
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-15 09:30:14 +0200

    Image Shape View added

---
+++ FlexViews/FlexImageShapeView.swift
@@ -0,0 +1,105 @@
+//
+//  FlexImageShapeView.swift
+//  MJRFlexStyleComponents
+//
+//  Created by Martin Rehder on 02/10/2016.
+/*
+ * Copyright 2016-present Martin Jacob Rehder.
+ * http://www.rehsco.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+import UIKit
+import StyledLabel
+import FlexControls
+
+public enum FlexImageShapeFit {
+    case center
+    case scaleToFill
+    case scaleToFit
+}
+
+open class FlexImageShapeView: FlexView {
+    fileprivate var backgroundShape = CALayer()
+    
+    open var image: UIImage? = nil {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    open var backgroundImageFit: FlexImageShapeFit = .scaleToFit {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    @objc open dynamic var imageStyle: FlexShapeStyle = FlexShapeStyle(style: .box) {
+        didSet {
+            self.setNeedsLayout()
+            imageStyle.styleChangeHandler = {
+                newStyle in
+                self.setNeedsLayout()
+            }
+        }
+    }
+    
+    @objc open dynamic var imageBorderColor: UIColor? = nil {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @objc open dynamic var imageBorderWidth: CGFloat = 1.0 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initView()
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        self.setupImageView()
+    }
+    
+    func setupImageView() {
+        let imageViewRect = self.getViewRect()
+        
+        if self.backgroundShape.superlayer != nil {
+            self.backgroundShape.removeFromSuperlayer()
+        }
+        
+        let bgLayer = ImageShapeLayerFactory.createImageShapeInView(imageViewRect, viewBounds: self.bounds, image: self.image, viewStyle: self.getStyle(), imageStyle: self.imageStyle.style, imageFitting: self.backgroundImageFit, borderWidth: self.imageBorderWidth, borderColor: self.imageBorderColor)
+        
+        self.styleLayer.insertSublayer(bgLayer, at: 0)
+        self.backgroundShape = bgLayer
+    }
+
+}
---
+++ FlexViews/Styling/ImageShapeLayerFactory.swift
@@ -0,0 +1,80 @@
+//
+//  ImageShapeLayerFactory.swift
+//  FlexViews
+//
+//  Created by Martin Rehder on 04.10.2016.
+/*
+ * Copyright 2016-present Martin Jacob Rehder.
+ * http://www.rehsco.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+import UIKit
+import StyledLabel
+
+open class ImageShapeLayerFactory {
+
+    public static func createImageShapeInView(_ imageViewRect: CGRect, viewBounds: CGRect, image: UIImage?, viewStyle: ShapeStyle, imageStyle: ShapeStyle, imageFitting: FlexImageShapeFit, borderWidth: CGFloat? = nil, borderColor: UIColor? = nil) -> CALayer {
+        let bgLayer = CALayer()
+        bgLayer.frame = imageViewRect
+        let clipRect = viewBounds.offsetBy(dx: -imageViewRect.origin.x, dy: -imageViewRect.origin.y)
+        let maskShapeLayer = StyledShapeLayer.createShape(viewStyle, bounds: clipRect, color: UIColor.black)
+        bgLayer.mask = maskShapeLayer
+        
+        if let img = image {
+            let imgLayer = ImageShapeLayerFactory.createImageShape(imageViewRect, image: img, imageStyle: imageStyle, imageFitting: imageFitting)
+            
+            if let bw = borderWidth, bw > 0 && borderColor != nil {
+                let bLayer = StyledShapeLayer.createShape(imageStyle, bounds: imgLayer.bounds.insetBy(dx: bw * 0.5, dy: bw * 0.5), color: .clear, borderColor: borderColor ?? .clear, borderWidth: bw)
+                imgLayer.addSublayer(bLayer)
+            }
+            bgLayer.addSublayer(imgLayer)
+        }
+        
+        return bgLayer
+    }
+    
+    public static func createImageShape(_ imageViewRect: CGRect, image: UIImage, imageStyle: ShapeStyle, imageFitting: FlexImageShapeFit) -> CALayer {
+        let imgLayer = CALayer()
+        var imgRect = CGRect(origin: CGPoint.zero, size: imageViewRect.size)
+        switch imageFitting {
+        case .center:
+            let iSize = image.size
+            let bgOffset = CGPoint(x: (imgRect.size.width - iSize.width) * 0.5, y: (imgRect.size.height - iSize.height) * 0.5)
+            imgRect = CGRect(x: bgOffset.x, y: bgOffset.y, width: iSize.width, height: iSize.height)
+        case .scaleToFill:
+            break
+        case .scaleToFit:
+            let imageR = CGRect(origin: CGPoint.zero, size: image.size)
+            imgRect = CGRectHelper.AspectFitRectInRect(imageR, rtarget: imgRect)
+        }
+        imgLayer.bounds = imgRect
+        imgLayer.contents = image.cgImage
+        imgLayer.position = CGPoint(x: imgRect.midX, y: imgRect.midY)
+        let maskPath = StyledShapeLayer.shapePathForStyle(imageStyle, bounds: imgRect)
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = maskPath.cgPath
+        imgLayer.mask = maskLayer
+        
+        return imgLayer
+    }
+
+}
